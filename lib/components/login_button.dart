5a8bd5093fbd05193aef03b7b9c396cd05509b4d
Sheeraz Ali <sheerazali7676@gmail.com>
2022-06-24 12:47:46 -0700

    MaterialButton

--- lib/components/login_button.dart
+++ lib/components/login_button.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+
 class LoginButton extends StatelessWidget {
   const LoginButton({
     Key? key,
@@ -7,29 +7,47 @@ class LoginButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('Login Succesfully');
-      },
-      child: Container(
-        height: 50,
-        width: 230,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.teal,
-        ),
-        child: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            'Login',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.white),
-          ),
-        ),
+    return MaterialButton(
+      onPressed: () {},
+      child: const Text(
+        'Login',
+        style: TextStyle(fontSize: 25, color: Colors.white),
       ),
+      color: Colors.teal,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      elevation: 5.0,
+      minWidth: 230.0,
+      height: 45,
     );
   }
 }
+
+
+// GestureDetector(
+//       onTap: () {
+//         print('Login Succesfully');
+//       },
+//       child: Card(
+//         elevation: 20,
+//         child: Container(
+//           height: 50,
+//           width: 230,
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(0),
+//             color: Colors.teal,
+//           ),
+//           child: const Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Text(
+//               'Login',
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                   fontSize: 25,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.white),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -51,4 +51,3 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
-
